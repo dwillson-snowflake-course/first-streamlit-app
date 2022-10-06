1889ee60ac3d58ae9c70b0f92770ad733cb4412d
dwillson-snowflake-course <115185318+dwillson-snowflake-course@users.noreply.github.com>
2022-10-06 15:59:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 myfruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 myfruitlist = myfruitlist.set_index("Fruit")
-streamlit.multiselect("Pick some fruits:",list(myfruitlist.index),['Avocado','Strawberries'])
-streamlit.dataframe(myfruitlist)
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(myfruitlist.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
