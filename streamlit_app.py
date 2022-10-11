99ddf996a6b68004948181514256b3fd129a830e
dwillson-snowflake-course <115185318+dwillson-snowflake-course@users.noreply.github.com>
2022-10-11 14:36:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ myfruitlist = myfruitlist.set_index("Fruit")
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(myfruitlist.index),['Avocado','Strawberries'])
 fruits_to_show = myfruitlist.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
